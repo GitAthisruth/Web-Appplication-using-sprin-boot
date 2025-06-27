6cb08192c878487fdf469895aebb06605378cb01
Athisruth <athisruthmsatheesh98@gmail.com>
2025-06-28 03:50:58 +0530

    remove center button and add defender text to all pages and remove some options from the drop down window

--- demo/src/main/resources/static/js/main.js
+++ demo/src/main/resources/static/js/main.js
@@ -1,1 +1,10 @@
 console.log("Main JS loaded.");
+
+document.addEventListener("DOMContentLoaded", () => {
+    const menuButton = document.getElementById("menu-button");
+    const menuDropdown = document.getElementById("menu-dropdown");
+
+    menuButton.addEventListener("click", () => {
+      menuDropdown.classList.toggle("hidden");
+    });
+  });
